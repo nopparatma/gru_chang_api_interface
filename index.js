271659aa5dd42762afc89bed38dbdb7bd2656417
Nopparat Manoi <yachty22541@gmail.com>
2023-11-21 23:34:43 +0700

    initial project

---
+++ index.js
@@ -0,0 +1,10 @@
+let express = require('express');
+let app = express();
+
+app.listen(8899, () => {
+    console.log("Server start at port 8899");
+})
+
+app.get("/api/welcome", (req, res, next) => {
+    res.json({msg: "Welcome eiei"});
+});
